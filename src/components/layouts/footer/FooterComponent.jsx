08e0cd7c4b36cb557a0a147b65a9400817a002ef
Andi Wicaksono Nugroho <75154388+Andiwn121@users.noreply.github.com>
2023-05-09 11:45:57 +0700

    resolve footer

--- src/components/layouts/footer/FooterComponent.jsx
+++ src/components/layouts/footer/FooterComponent.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Layout } from "antd";
 
 const FooterComponent = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const { Footer } = Layout;
+
+  return (
+    <Footer
+      style={{
+        textAlign: "center",
+        backgroundColor:"#4b0082",
+        color: "#fff"
+      }}
+    >
+      SonicSpectrum ©2023 Created by Andi Wicaksono Nugroho
+    </Footer>
+  );
+};
 
 export default FooterComponent;
